f8b6d3aac10e700cf9e06a64f9cff2a6e36be2b0
Carldric Clement <46277745+CarldricGaming@users.noreply.github.com>
2023-03-08 18:24:13 +0800

    update script

--- ISS/Resource/IC_Protect/IC_Protect.bat
+++ ISS/Resource/IC_Protect/IC_Protect.bat
@@ -7,6 +7,6 @@ timeout 2 > nul
 
 title Packing IC Protect...
 
-7za.exe a -bb3 -mmt2 -mx9 -i@ISD_List.txt -p"DontTouch" Setup.db
+uha.exe a -r+ -ed- -p- -m1 -mm+ -md+ -md32768 -b32768 Setup.db "ISDone_resource\*" 
 
 exit
