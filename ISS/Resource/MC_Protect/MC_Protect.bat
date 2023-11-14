192bdab7a3d974bfe3898dc55a828d4406458230
Carldric Clement <46277745+CarldricGaming@users.noreply.github.com>
2023-11-14 22:40:34 +0800

    Add files via upload

--- ISS/Resource/MC_Protect/MC_Protect.bat
+++ ISS/Resource/MC_Protect/MC_Protect.bat
@@ -11,12 +11,12 @@ timeout 2 > nul
 
 title Packing ISDone Protect...
 
-uha.exe a -r+ -ed- -p- -m1 -mm+ -md+ -md32768 -b32768 Setup.db "ISDone_resource\*" @ISD_List.txt
+ISD_List.exe
 
 exit
 
 :ISDManualSFX
 
-uha.exe a -r+ -ed- -p- -m1 -mm+ -md+ -md32768 -b32768 Setup.db "ISDone_resource\*" @ISD_List_Manual_SFX.txt
+ISD_List_Manual_SFX.exe
 
 exit
